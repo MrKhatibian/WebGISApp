a40f073405ea4ed2bafe140d6e199e7548ca682d
MrKhatibian <mr.khatibian98@gmail.com>
2024-09-30 14:37:22 +0330

    File Projec Edited

--- WebGISApp/App_Start/BundleConfig.cs
+++ WebGISApp/App_Start/BundleConfig.cs
@@ -20,7 +20,8 @@ namespace WebGISApp
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/Map.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
--- WebGISApp/Content/Map.js
+++
@@ -1,6 +0,0 @@
-﻿// Configure the asset path to your desired directory for components and core
-import { setArcgisAssetPath as setMapAssetPath } from '@arcgis/map-components/dist/components';
-import esriConfig from "@arcgis/core/config.js";
-
-setMapAssetPath("./assets");
-esriConfig.assetsPath = "./assets";
