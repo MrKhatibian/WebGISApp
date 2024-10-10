3b364f8475e16f4c04250cbcd28362f9bbcdfe90
MrKhatibian <mr.khatibian98@gmail.com>
2024-10-10 11:37:17 +0330

    Move Zoom Buttons

---
+++ WebGISApp/Scripts/Map/LoadMap copy.js
@@ -0,0 +1,38 @@
+﻿require(
+    [
+     "esri/config",
+     "esri/Map",
+     "esri/views/MapView",
+     "esri/layers/MapImageLayer",
+     "esri/layers/FeatureLayer",
+     "esri/webdoc/applicationProperties/Search"
+    ],
+    function(esriConfig, Map, MapView, MapImageLayer, FeatureLayer,Search) 
+    {        
+        const map = new Map({
+        //basemap: "arcgis/topographic" // basemap styles service
+        });
+      
+        // Adding a sample Map Image Layer
+        var layer = new MapImageLayer({
+        url: "http://localhost:6080/arcgis/rest/services/Maryanaj/Maryanaj_14030619/MapServer" // Replace with your ArcGIS Server URL         
+        });
+        map.add(layer);
+
+        const featureLayer = new FeatureLayer("http://localhost:6080/arcgis/rest/services/SampleWorldCities/MapServer/1");
+        //map.add(featureLayer);
+
+        const view = new MapView({
+        map: map,
+        center: [48.464869, 34.834155], // Longitude, latitude 48.464869  34.834155
+        zoom: 16, // Zoom level
+        container: "mapView" // Div element
+        });
+
+        const searchWidget = new Search({view:view});
+        //view.ui.add(searchWidget, "top-right");
+        
+        //view.ui.move(["zoom",botton-left]);
+
+    }
+);
--- WebGISApp/Scripts/Map/LoadMap.js
+++ WebGISApp/Scripts/Map/LoadMap.js
@@ -1,31 +1,48 @@
-﻿require(
-    [
-     "esri/config",
-     "esri/Map",
-     "esri/views/MapView",
-     "esri/layers/MapImageLayer",
-     "esri/layers/FeatureLayer"
-    ],
-    function(esriConfig, Map, MapView, MapImageLayer, FeatureLayer) 
-    {        
-        const map = new Map({
-        //basemap: "arcgis/topographic" // basemap styles service
-        });
-      
-        // Adding a sample Map Image Layer
-        var layer = new MapImageLayer({
-        url: "http://localhost:6080/arcgis/rest/services/Maryanaj/Maryanaj_14030619/MapServer" // Replace with your ArcGIS Server URL         
-        });
-        map.add(layer);
+﻿window.onload = function(){
+    require(
+        [
+         "esri/Map",
+         "esri/views/MapView",
+         "esri/layers/MapImageLayer",
+         "esri/layers/FeatureLayer",     
+        ],
+        function(
+                Map, MapView, MapImageLayer            
+            ) 
+        {        
+            let map = new Map({
+            //basemap: "arcgis/topographic" // basemap styles service
+            });
+          
+            // Adding a sample Map Image Layer
+            let layer = new MapImageLayer({
+            url: "http://localhost:6080/arcgis/rest/services/Maryanaj/Maryanaj_14030619/MapServer" // Replace with your ArcGIS Server URL         
+            });
+            map.add(layer);
+    
+            //const featureLayer = new FeatureLayer("http://localhost:6080/arcgis/rest/services/SampleWorldCities/MapServer/1");
+            //map.add(featureLayer);
+    
+            let view = new MapView({
+            map: map,
+            center: [48.464869, 34.834155], // Longitude, latitude 48.464869  34.834155
+            zoom: 18, // Zoom level
+            container: "mapView" // Div element
+            });
+    
+            //const searchWidget = new Search({view:view});
+            //view.ui.add(searchWidget, "top-right");        
+            view.ui.move("zoom","bottom-right");
+            document.getElementById("zoomIn").onclick = function(){
+                //view.zoom += 1;
+                //alert("Hiii");
+                view.goto({
+                    center:[48.464869, 34.834155],
+                    zoom:16
+                });
+            }
+    
+        }
+    );
+};
 
-        const featureLayer = new FeatureLayer("http://localhost:6080/arcgis/rest/services/SampleWorldCities/MapServer/1");
-        //map.add(featureLayer);
-
-        const view = new MapView({
-        map: map,
-        center: [48.464869, 34.834155], // Longitude, latitude 48.464869  34.834155
-        zoom: 16, // Zoom level
-        container: "mapView" // Div element
-        });
-    }
-);
